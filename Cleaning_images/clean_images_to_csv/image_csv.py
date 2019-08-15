9e86b25eb3dea23ff4cd4b0987cde279b30f5b81
Divija Palleti <IHM2016005@iiita.ac.in>
2019-08-15 15:30:07 +0530

    Fixed file path bug

--- Cleaning_images/clean_images_to_csv/image_csv.py
+++ Cleaning_images/clean_images_to_csv/image_csv.py
@@ -7,7 +7,7 @@ col = '_file_'
 
 
 for index, row in df.iterrows():
-    rep = str("newDataset/" + str(row["dir"]) + "_"+str(row["id"])+".BMP")
+    rep = str("" + str(row["dir"]) + "_"+str(row["id"])+".BMP")
     print(rep)
     row[col]  = rep
     df.at[index,col] = rep
